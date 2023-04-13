1ed2b4794b165d8c87926ee8e0d3c5ba6893a4d9
David Fales <davidfalesct@gmail.com>
2023-04-13 11:03:24 -0600

    added startup code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import React, { useState } from 'react'
+import NavBar from '../components/Navbar'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <div>
+      <Head>
+        <title>Lit Line</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* <link rel="icon" href="./static/favicon.ico" /> */}
+      </Head>
+      <Header />
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
+export default MyApp
